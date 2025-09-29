dd6bd60fda72ec2e50b16fdee589c41238eb468b
HarshalJ96k <harshalj96k@gmail.com>
2025-09-29 08:01:07 +0530

    first commit

---
+++ If_ElseIf.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int marks;
+    cout << "Enter marks: ";
+    cin >> marks;
+    if(marks >= 90) {
+        cout << "Grade A";
+    } else if(marks >= 70) {
+        cout << "Grade B";
+    } else if(marks >= 50) {
+        cout << "Grade C";
+    } else if(marks >= 30) {
+        cout << "Grade D";
+    } else {
+        cout << "Fail";
+    }
+    return 0;
+}
