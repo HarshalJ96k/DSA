f8d44dcdad4c05dcb4204cb4aa2335adacfb1ffa
HarshalJ96k <harshalj96k@gmail.com>
2026-02-09 23:09:29 +0530

    09/02/2026

--- Learn The Basics/Logical Thinking-Patterns/pattern_18.cpp
+++ Learn The Basics/Logical Thinking-Patterns/pattern_18.cpp
@@ -5,9 +5,11 @@ void pattern(int n)
 {
     for (int i=1;i<=n;i++)
     {
-        for(char ch='E';ch>'E'-i;ch--)
+        char ch='E'-i+1;
+        for(int j=1;j<=i;j++)
         {
             cout<<ch;
+            ch++;
         }
         cout<<endl;
     }
---
+++ Learn The Basics/Logical Thinking-Patterns/pattern_19.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+void pattern(int n)
+{
+     int space=0;
+    for (int i=0;i<n;i++)
+    {
+       
+        for(int j=0;j<n-i;j++)
+        {
+            cout<<"*";
+        }
+        for (int j=0;j<space;j++)
+        {
+
+            cout<<" ";
+           
+        }
+        for(int j=0;j<n-i;j++)
+        {
+            cout<<"*";
+        }
+         space+=2;
+        cout<<endl;
+    }
+
+    space=8;
+    for (int i=0;i<n;i++)
+    {
+       
+        for(int j=0;j<=i;j++)
+        {
+            cout<<"*";
+        }
+        for (int j=0;j<space;j++)
+        {
+
+            cout<<" ";
+           
+        }
+        for(int j=0;j<=i;j++)
+        {
+            cout<<"*";
+        }
+         space-=2;
+        cout<<endl;
+    }
+}
+int main ()
+{
+    int n;
+    cout<<"Enter the No of Rows and Columns:";
+    cin>>n;
+    pattern(n);
+    return 0;
+}
